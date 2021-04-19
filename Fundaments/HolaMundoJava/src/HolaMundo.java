2ee3e309d69a93c2d5f4f31ac93e02b307111f1b
Leandro J <leandrojulian1391@gmail.com>
2021-04-19 20:49:05 -0300

    Added Integer Variable example

--- Fundaments/HolaMundoJava/src/HolaMundo.java
+++ Fundaments/HolaMundoJava/src/HolaMundo.java
@@ -10,6 +10,7 @@
  */
 public class HolaMundo {
     public static void main(String args[]){
-        System.out.println("Hola Mundo desde Java");
+         int myVariableInteger = 10;
+         System.out.println(myVariableInteger);
     }
 }
