9c7fa5cd25a31d47f5cf6fd4a5ab40d3719d62f8
Leandro J <leandrojulian1391@gmail.com>
2021-04-19 21:03:12 -0300

    Added exmple with String - Type Class on Java

--- Fundaments/HolaMundoJava/src/HolaMundo.java
+++ Fundaments/HolaMundoJava/src/HolaMundo.java
@@ -10,7 +10,22 @@
  */
 public class HolaMundo {
     public static void main(String args[]){
+//        Define the variable
          int myVariableInteger = 10;
          System.out.println(myVariableInteger);
+         
+//         Modificamos el valor de la variable
+         myVariableInteger = 5;
+         System.out.println(myVariableInteger);
+         
+         // String is a class in Java, not Type Primitive
+         String  myVariableString = "Greeteings";
+         System.out.println(myVariableString);
+         
+         //We modified the string variable
+          myVariableString = "Goodbye";
+         System.out.println(myVariableString);
+         
+         
     }
 }
