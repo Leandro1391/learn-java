ec0efc4b71cdcf4aa393d976275e6db827a3934c
Leandro J <leandrojulian1391@gmail.com>
2021-04-21 19:48:50 -0300

    Use list unicode in char and integer types

--- Fundaments/HolaMundoJava/src/HolaMundo.java
+++ Fundaments/HolaMundoJava/src/HolaMundo.java
@@ -14,15 +14,46 @@ public class HolaMundo {
 
     public static void main(String args[]) {
         
-        //inference is int
-        var numberInteger = 10;
-        System.out.println("numberInteger = " + numberInteger);
-     
-        var numberDouble  = 10.0;
-        System.out.println("numberDouble = " + numberDouble);
+        // char value store 16 bits like short
+        char myCharacter = 'a';
+        System.out.println("myCharacter = " + myCharacter);
         
-        //Por defecto es double para forzar hay que aplicar F
-        var numberFloat = 10.0F;
-        System.out.println("numberFloat = " + numberFloat);
+        char variableChar = '\u0021';
+        System.out.println("variableChar = " + variableChar);
+        
+        char varCharDecimal = 33;
+        System.out.println("carCharDecimal = " + varCharDecimal);
+        
+        char varCharSymbol = '!';
+        System.out.println("varCharSymbol = " + varCharSymbol);
+        
+        //With declaration var -  inference
+        var varChar0 = '\u0021';
+        System.out.println("varChar0 = " + varChar0);
+        
+        //in this case will not be to char, inferece will take to int
+        var varCharDecimal1 = 33;
+        System.out.println("varCharDecimal1 = " + varCharDecimal1);
+        
+        //var char explicit parse
+        var varCharDecimalExplicitParse = (char) 33;
+        System.out.println("varCharDecimalExplicitParse = " + varCharDecimalExplicitParse);
+        
+        var varCharSymbol2= '!';
+        System.out.println("varCharSymbol2 = " + varCharSymbol2);
+        
+        //example with int
+        int variableIntegerSymbol = '!';
+        System.out.println("variableIntegerSymbol = " + variableIntegerSymbol);
+        
+        int  letter = 'a';
+        System.out.println("letter = " + letter);
+        
+        char letter2 = 97;
+        System.out.println("letter2 = " + letter2);
+        
+        int  letterMayus = 'A';
+        System.out.println("letterMayus = " + letterMayus);
+      
     }
 }
