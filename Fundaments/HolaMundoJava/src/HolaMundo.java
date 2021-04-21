eb5ab8ae76587dade3757542feca44dc681ba1b2
Leandro J <leandrojulian1391@gmail.com>
2021-04-21 19:19:53 -0300

    Inference with type float and int

--- Fundaments/HolaMundoJava/src/HolaMundo.java
+++ Fundaments/HolaMundoJava/src/HolaMundo.java
@@ -14,34 +14,15 @@ public class HolaMundo {
 
     public static void main(String args[]) {
         
-        // Todo estas clases están contenidas en la clase Java.lang
-
-        /* Architectura del procesador el adecuado en mi caso en long porque tengo AMD64
-            tipos primitivos de tipo flotante: float(32bits) y double (64bits)
-         */
-        
-        float numberFLoat = 10.0f;
-        System.out.println("numberFLoat = " + numberFLoat);
-        float numberFLoat1 = (float)22.2;
-        System.out.println("numberFLoat1 = " + numberFLoat1);
-        
-        System.out.println("Minimun vlaue type float: " + Float.MIN_VALUE);
-        System.out.println("Maximun vlaue type float: " + Float.MAX_VALUE);
-        
-        //explicit convertions
-        float numberFLoatOverload = (float)3.4028235E38;
-        System.out.println("numberFLoatOverload = " + numberFLoatOverload);
-        float numberFLoatOverload2 = (float)3.4028236E38;
-        System.out.println("numberFLoatOverload2 = " + numberFLoatOverload2);
-        
-        double numberDouble = 10;
+        //inference is int
+        var numberInteger = 10;
+        System.out.println("numberInteger = " + numberInteger);
+     
+        var numberDouble  = 10.0;
         System.out.println("numberDouble = " + numberDouble);
         
-        System.out.println("Minimun vlaue type double: " + Double.MIN_VALUE);
-        System.out.println("Maximun vlaue type double: " + Double.MAX_VALUE);
-        
-        //The maximal is type Double
-        
-        
+        //Por defecto es double para forzar hay que aplicar F
+        var numberFloat = 10.0F;
+        System.out.println("numberFloat = " + numberFloat);
     }
 }
