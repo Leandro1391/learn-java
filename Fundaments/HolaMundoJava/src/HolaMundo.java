4bb990141879f7fbeaa19ac4094f390e817a0abf
Leandro J <leandrojulian1391@gmail.com>
2021-04-22 22:55:43 -0300

    Ternary operator example

--- Fundaments/HolaMundoJava/src/HolaMundo.java
+++ Fundaments/HolaMundoJava/src/HolaMundo.java
@@ -14,27 +14,14 @@ public class HolaMundo {
 
       public static void main(String args[]) {
 
-            //Conditional Operators
-            var a = 5;
-            var minValue = 0;
-            var maxValue = 10;
+            var result = (3 > 2) ? "true" : "false";
+            System.out.println("result = " + result);
+            
+            // Solamente se usan si son expresiones sencillas el operador ternario
+            
+            var number = 5;
+            result = (number % 2 == 0) ? "Es par" : "Es impar";
+            System.out.println("result = " + result);
 
-            var result = a >= 0 && a <= 10;
-            if (result) {
-                  System.out.println("inside of range");
-            } else {
-                  System.out.println("Out of range");
-            }
-
-//            if (a > = 0 && a <= 10)
-//                  System.out.println("a is  in the range between 0 and 10");
-            var holidays = true;
-            var dayRest = false;
-
-            if (holidays || dayRest) {
-                  System.out.println("the father can to asist son's game");
-            } else {
-                  System.out.println("the father is bussy");
-            }
       }
 }
