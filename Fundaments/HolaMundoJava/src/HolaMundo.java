9ef0cd142c0b01e49507434e550fb63325c25a0d
Leandro J <leandrojulian1391@gmail.com>
2021-04-22 22:03:41 -0300

    Unary operators in java

--- Fundaments/HolaMundoJava/src/HolaMundo.java
+++ Fundaments/HolaMundoJava/src/HolaMundo.java
@@ -11,22 +11,45 @@ import java.util.Scanner;
  * @author leandro
  */
 public class HolaMundo {
-
-    public static void main(String args[]) {
-        
-       int a = 3, b=2;
-       int c = a + 5 -b;
-        System.out.println("c = " + c);
-        
-        a += 1; //a = a + 1;
-        System.out.println("a = " + a);
-        
-        a += 3; //a = a + 3
-        System.out.println("a = " + a);
-        
-        a -=2; // a= a -2;
-        System.out.println("a = " + a);
-        
-        // *= /=  *= %=
-    }
+      
+      public static void main(String args[]) {
+            //Numbers
+            var a = 3;
+            var b = -a;
+            System.out.println("a = " + a);
+            System.out.println("b = " + b);
+            
+            //Boolean
+            var c = true;
+            var d = !c;       //negation
+            System.out.println("c = " + c);
+            System.out.println("d = " + d);
+            
+            //increment
+            //1.preincrement (symbole goes before  of the variable)
+            var e = 3;
+            var f = ++e; //first increment the variable and later use his value
+            System.out.println("e = " + e);
+            System.out.println("f = " + f);
+            
+            //2.postincrement (symbol goes after the variable)
+            var g = 5;
+            var h = g++; //h = 5
+            System.out.println("g = " + g); //g = 6 increment
+            System.out.println("h = " + h); // h=5
+            
+            //decrement
+            //1.predecrement
+            var i = 2;
+            var j = --i;
+            System.out.println("i = " + i);
+            System.out.println("j = " + j);
+            
+            //2.Postdecrement
+            var k =4;
+            var l= k--; // first use the variable value and stay pending th decrement
+            System.out.println("k = " + k); //has pending one decrement
+            System.out.println("l = " + l); 
+            
+      }
 }
