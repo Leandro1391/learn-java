cb9963cf98cf5de49acd35024548fce2d534f5c4
Leandro J <leandrojulian1391@gmail.com>
2021-04-24 10:08:47 -0300

    Sentencia if - else

--- Fundaments/HolaMundoJava/src/HolaMundo.java
+++ Fundaments/HolaMundoJava/src/HolaMundo.java
@@ -14,25 +14,14 @@ public class HolaMundo {
 
       public static void main(String args[]) {
 
-            var numero1 = 0;
-            var numero2 = 0;
+            //Sentece if  else
+            var condition = true;
 
-            var console = new Scanner(System.in);
-
-            System.out.println("Proprociona el numero1: ");
-            numero1 = Integer.parseInt(console.nextLine());
-
-            System.out.println("Proporciona el numero2: ");
-            numero2 = Integer.parseInt(console.nextLine());
-            System.out.println("El numero mayor es: ");
-            System.out.println(String.valueOf( numero1>numero2 ? numero1 : numero2==numero1 ? "Error: Los numeros son iguales" : numero2));
-
-//            if (numero1 > numero2) {
-//                  System.out.println("El numero mayor es: " + numero1);
-//            } else if (numero1 == numero2) {
-//                  System.out.println("Los numeros son iguales");
-//            } else {
-//                  System.out.println("El numeros mayor es: " + numero2);
-//            }
+            if (condition) {
+                  System.out.println("True condition");
+                  System.out.println("New Line");
+            } else {
+                  System.out.println("False condition");
+            }
       }
 }
