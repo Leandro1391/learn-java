b7c48ff988430cabcee5498a17811b2b42eeb2a4
Leandro J <leandrojulian1391@gmail.com>
2021-04-24 17:56:12 -0300

    Labels in Java

--- Fundaments/HolaMundoJava/src/HolaMundo.java
+++ Fundaments/HolaMundoJava/src/HolaMundo.java
@@ -14,11 +14,14 @@ public class HolaMundo {
 
       public static void main(String args[]) {
 
+            //NO ES RECOMENDABLE USAR GO TO (ETiQUETAS - LABELS)
             
-            //continue va a la siguiente iteración del ciclo for, no ejecuta las lineas que faltan en el ciclo
+            //En este caso lo que hace es cuando ejecuta la linea 24 se va a la 21, se puede usar cuando queramos ir a un for previo, es decir, ciclos anidados
+            
+            inicio:
             for (int contador = 0; contador < 3; contador++) {
                   if (contador % 2 != 0) {
-                        continue;//go to the next iteration
+                        continue inicio;//go to the code line label (21)
                   }
                   System.out.println("contador = " + contador);
             }
@@ -26,7 +29,7 @@ public class HolaMundo {
 //            for (int contador = 0; contador < 3; contador++) {
 //                  if (contador % 2 == 0) {
 //                        System.out.println("contador = " + contador);
-//                        break;
+//                        break inicio;
 //                  }
 //            }
 
