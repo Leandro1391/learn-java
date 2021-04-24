099403d1176f75ee70e851f62310f9a37b3e4033
Leandro J <leandrojulian1391@gmail.com>
2021-04-24 17:15:09 -0300

    break and continue examples

--- Fundaments/HolaMundoJava/src/HolaMundo.java
+++ Fundaments/HolaMundoJava/src/HolaMundo.java
@@ -13,10 +13,22 @@ import java.util.Scanner;
 public class HolaMundo {
 
       public static void main(String args[]) {
+
             
+            //continue va a la siguiente iteración del ciclo for, no ejecuta las lineas que faltan en el ciclo
             for (int contador = 0; contador < 3; contador++) {
+                  if (contador % 2 != 0) {
+                        continue;//go to the next iteration
+                  }
                   System.out.println("contador = " + contador);
             }
+            
+//            for (int contador = 0; contador < 3; contador++) {
+//                  if (contador % 2 == 0) {
+//                        System.out.println("contador = " + contador);
+//                        break;
+//                  }
+//            }
 
 //            var contador = 0;
 //            while(contador < 3) {
@@ -29,6 +41,5 @@ public class HolaMundo {
 //                  System.out.println("contador = " + contador);
 //                  contador++;
 //            } while (contador < 3);
-
       }
 }
