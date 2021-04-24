f5e3a9ba31d305298c98aec29b08fe9fd6a973ce
Leandro J <leandrojulian1391@gmail.com>
2021-04-24 14:18:42 -0300

    Example with Sentence switch

--- Fundaments/HolaMundoJava/src/HolaMundo.java
+++ Fundaments/HolaMundoJava/src/HolaMundo.java
@@ -13,34 +13,29 @@ import java.util.Scanner;
 public class HolaMundo {
 
       public static void main(String args[]) {
-
-            var mounth = 0;
-            var console = new Scanner(System.in);
-            var season = "Season unknow";
-            var ok = 'n';
-
-            do {
-                  System.out.println("Press the number between 1 to 12  for know the season");
-                  mounth = Integer.parseInt(console.nextLine());
-
-                  if (mounth == 1 || mounth == 2 || mounth == 12) {
-                        season = "Winter";
-                  } else if (mounth == 3 || mounth == 4 || mounth == 5) {
-                        season = "Spring";
-                  } else if (mounth == 6 || mounth == 7 || mounth == 8) {
-                        season = "Summer";
-                  } else if (mounth == 9 || mounth == 10 || mounth == 11) {
-                        season = "Autumn";
-                  } else {
-                        System.out.println("Wrong number please try again");
-                        season = "Season unknow";
-                  }
-                  System.out.println("season = " + season);
-                  
-                  System.out.println("Do you want continue? y/n");
-                  ok = console.nextLine().charAt(0);
-
-            } while (mounth <= 0 || mounth >= 13 || ok == 'y');
-
+            
+            // Switch is the best way for menú options
+            //IF I don't added the keyword break the ejecution procedes to the next case in the switch
+            
+            var number = 4;
+            var numberText = "unknow value";
+            
+            switch (number){
+                  case 1:
+                          numberText = "Number one";
+                          break;
+                  case 2:
+                        numberText = "Number two";
+                        break;
+                  case 3:
+                        numberText = "Number three";
+                        break;
+                  case 4:
+                        numberText = "Number four";
+                        break;
+                  default:
+                        numberText = "Case not found";
+            }
+            System.out.println("numberText = " + numberText);
       }
 }
