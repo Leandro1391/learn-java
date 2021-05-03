7229ce9218db83b03b7a809fb50d004d9b96a210
Leandro Julian <leandrojulian1391@gmail.com>
2021-05-03 19:45:45 -0300

    Data type solution

--- Fundaments/hackerRankTest/src/hackerranktest/HackerRankTest.java
+++ Fundaments/hackerRankTest/src/hackerranktest/HackerRankTest.java
@@ -9,35 +9,30 @@ public class HackerRankTest {
        */
       public static void main(String[] args) {
 
-            Scanner in = new Scanner(System.in);
+            {
 
-            int q, a, b, n, result = 0, p = 1;
+                  Scanner sc = new Scanner(System.in);
+                  int t = sc.nextInt();
 
-            do {
-                  q = in.nextInt();
-            } while (q < 0 || q > 500);
+                  for (int i = 0; i < t; i++) {
 
-            for (int i = 0; i < q; i++) {
-
-                  do {
-                        a = in.nextInt();
-                        b = in.nextInt();
-                        n = in.nextInt();
-                  } while ((a < 0 || a > 50) || (b < 0 || b > 50) || (n < 1 || n > 15));
-
-                  for (int j = 0; j < n; j++) {
-
-                        if (j == 0) {
-                              result = a + (1) * b;
-                        } else {
-                              p *= 2;
-                              result += p * b;
+                        try {
+                              long x = sc.nextLong();
+                              System.out.println(x + " can be fitted in:");
+                              if (x >= Byte.MIN_VALUE && x <= Byte.MAX_VALUE) {
+                                    System.out.println("* byte \n* short \n* int\n* long");
+                              } else if (x >= Short.MIN_VALUE && x <= Short.MAX_VALUE) {
+                                    System.out.println("* short\n* int \n* long");
+                              } else if (x >= Integer.MIN_VALUE && x <= Integer.MAX_VALUE) {
+                                    System.out.println("* int\n* long");
+                              } else if (x >= Long.MIN_VALUE && x <= Long.MAX_VALUE) {
+                                    System.out.println("* long");
+                              } 
+                        } catch (Exception e) {
+                              System.out.println(sc.next() + " can't be fitted anywhere.");
                         }
-                        System.out.print(result + " ");
+
                   }
-                  System.out.println("");
-                  p = 1;
             }
-            in.close();
       }
 }
