528967b0b5cc2dc08f4104271ece0b6ef2cba3c8
Leandro Julian <leandrojulian1391@gmail.com>
2021-05-06 20:52:58 -0300

    Add test in constructor void

--- Fundaments/Box/src/box/TestBox.java
+++ Fundaments/Box/src/box/TestBox.java
@@ -4,9 +4,18 @@ public class TestBox {
       
       public static void main(String[] args) {
             
-            //Create instance of object Box
-            Box box1 = new Box(3, 2, 6);
+            int heightMeasure = 8, widhtMeasure = 10,  depthMeasure= 2;
             
-            System.out.println("The box's volume is " + box1.calculateBoxVolume() + "mts");
+            Box box1 = new Box();
+            box1.depth = heightMeasure;
+            box1.height = widhtMeasure;
+            box1.width = depthMeasure;
+            
+            System.out.println("The box1's volume is " + box1.calculateBoxVolume() + " \u33A5");
+            
+            //Create instance of object Box
+            Box box2 = new Box(3, 2, 6);
+            
+            System.out.println("The box2's volume is " + box2.calculateBoxVolume() + " \u33A5");
       }
 }
