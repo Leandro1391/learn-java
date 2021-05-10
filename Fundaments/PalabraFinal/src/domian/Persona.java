a646fc5a0f5e781bb512412dc9dec2f5150fc90b
Leandro Julian <leandrojulian1391@gmail.com>
2021-05-10 20:41:18 -0300

    Definicion de un atributo constante en java

--- Fundaments/PalabraFinal/src/domian/Persona.java
+++ Fundaments/PalabraFinal/src/domian/Persona.java
@@ -3,6 +3,10 @@ package domian;
 //Si tiene la palabra reservada finla no podra extenderse a las clases hijas
 //public final class Persona {
 public final class Persona {
+      
+      //Esto se define como variable constante en Java porque es estatico, publico y final
+      public final static int MI_CONSTANTE = 1;
+      
       //No podremos modificar el comportamiento del metodo imprimir en una clase hija
       public final void imprimir() {
             System.out.println("Metodo imprimir");
--- Fundaments/PalabraFinal/src/test/TestFinal.java
+++ Fundaments/PalabraFinal/src/test/TestFinal.java
@@ -1,10 +1,16 @@
 package test;
 
+import domian.Persona;
+
 public class TestFinal {
       public static void main(String[] args) {
             final int miVariable = 10;
             System.out.println("miVariable = " + miVariable);
             // si declaramo una variable como final no podemos  cambiar su valor después, será constante en toda la ejecucion
 //            miVariable = 5;
+            
+            //No se puede modificar el valor ya asignado en un atributo contsntane
+//            Persona.MI_CONSTANTE = 5;
+            System.out.println("Persona.MI_CONSTANTE = " + Persona.MI_CONSTANTE);
       }
 }
