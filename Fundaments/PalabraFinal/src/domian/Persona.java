5ae5f58eead668b916557a4595699915ff7fb87b
Leandro Julian <leandrojulian1391@gmail.com>
2021-05-10 20:48:12 -0300

    Uso de final en objetos

--- Fundaments/PalabraFinal/src/domian/Persona.java
+++ Fundaments/PalabraFinal/src/domian/Persona.java
@@ -7,6 +7,18 @@ public final class Persona {
       //Esto se define como variable constante en Java porque es estatico, publico y final
       public final static int MI_CONSTANTE = 1;
       
+      private String nombre;
+
+      public String getNombre() {
+            return this.nombre;
+      }
+
+      public void setNombre(String nombre) {
+            this.nombre = nombre;
+      }
+      
+      
+      
       //No podremos modificar el comportamiento del metodo imprimir en una clase hija
       public final void imprimir() {
             System.out.println("Metodo imprimir");
--- Fundaments/PalabraFinal/src/test/TestFinal.java
+++ Fundaments/PalabraFinal/src/test/TestFinal.java
@@ -12,5 +12,16 @@ public class TestFinal {
             //No se puede modificar el valor ya asignado en un atributo contsntane
 //            Persona.MI_CONSTANTE = 5;
             System.out.println("Persona.MI_CONSTANTE = " + Persona.MI_CONSTANTE);
+            
+            
+            final Persona persona1 = new Persona();
+            
+            //No podemos volver asignar un referencia en memoria al objeto persona1 porque fue declarado como final
+//            persona1 = new Persona();
+            persona1.setNombre("Juan");
+            System.out.println("Persona1 nombre: " + persona1.getNombre());
+            persona1.setNombre("Carlos");
+            System.out.println("Persona1 nombre: " + persona1.getNombre());
+            //no se puede asginar una nueva referencia a la variable persona1 o no se puede cambiar la referencia pero sí podemos modificar el contenido a la que apunta como el atributo nombre
       }
 }
