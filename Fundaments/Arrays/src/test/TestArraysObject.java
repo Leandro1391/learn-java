0fdc94510c2449cf015a98bd28ea4297be991a2f
Leandro Julian <leandrojulian1391@gmail.com>
2021-05-10 22:33:02 -0300

    cycle for with array object

--- Fundaments/Arrays/src/test/TestArraysObject.java
+++ Fundaments/Arrays/src/test/TestArraysObject.java
@@ -12,5 +12,10 @@ public class TestArraysObject {
             personas[1] = new Persona("Pedro");
             System.out.println("personas 0 =  " + personas[0]);
             System.out.println("personas 1 =  " + personas[1]);
+            
+            for (int i = 0; i < personas.length; i++) {
+                  System.out.println("personas  " + i  + " = " + personas[i]);
+                  
+            }
       }
 }
