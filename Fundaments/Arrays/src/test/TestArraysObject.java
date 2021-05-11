746db925d2c4155c154c0f400e5d3e521009611e
Leandro Julian <leandrojulian1391@gmail.com>
2021-05-10 22:30:30 -0300

    Create array object and access elements

---
+++ Fundaments/Arrays/src/test/TestArraysObject.java
@@ -0,0 +1,16 @@
+package test;
+
+import domain.Persona;
+
+public class TestArraysObject {
+
+      public static void main(String[] args) {
+            //Los arrays se denominan en plural
+            Persona personas[] = new Persona[2];
+            
+            personas[0] = new Persona("Juan");
+            personas[1] = new Persona("Pedro");
+            System.out.println("personas 0 =  " + personas[0]);
+            System.out.println("personas 1 =  " + personas[1]);
+      }
+}
