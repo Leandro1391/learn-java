d607acb1ff71237a10d7a18de4b4d1d8ae8986c2
Leandro Julian <leandrojulian1391@gmail.com>
2021-05-10 22:06:25 -0300

    Modificar y acceder a un elemento del array

--- Fundaments/Arrays/src/test/TestArrays.java
+++ Fundaments/Arrays/src/test/TestArrays.java
@@ -7,6 +7,14 @@ public class TestArrays {
             //Los arreglos son de tipo Object y no se puede modificar el tamaó del array ya una vez declarado
             int ages[] = new int[3];
             System.out.println("ages = " + ages);
+            
+            //Modificar y acceder elementos d eun array
+            ages[0] = 10;
+            System.out.println("ages 0 = " + ages[0]);
+            ages[1] = 15;
+            System.out.println("ages 1 = " + ages[1]);
+            ages[2] = 27;
+            System.out.println("ages 2 = " + ages[2]);
       }
       
 }
