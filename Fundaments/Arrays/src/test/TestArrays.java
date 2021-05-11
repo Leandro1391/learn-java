cc41d49cd6e336cbe423c2d69df14ddeb7d2c6ea
Leandro Julian <leandrojulian1391@gmail.com>
2021-05-10 21:02:40 -0300

    Declaracion de un arreglo en java

---
+++ Fundaments/Arrays/src/test/TestArrays.java
@@ -0,0 +1,12 @@
+package test;
+
+public class TestArrays {
+      
+      public static void main(String[] args) {
+            //tambien se puede declarar así int []ages
+            //Los arreglos son de tipo Object y no se puede modificar el tamaó del array ya una vez declarado
+            int ages[] = new int[3];
+            System.out.println("ages = " + ages);
+      }
+      
+}
