afa3b4c7a4f0452032c44f707b4f8a8732ee6a41
Leandro Julian <leandrojulian1391@gmail.com>
2021-05-11 21:36:54 -0300

    Declaramos una matriz de un objeto tipo entero

---
+++ Fundaments/Matrices/src/test/TestMatrices.java
@@ -0,0 +1,11 @@
+package test;
+
+// Una matrz es una arreglo de arreglo en programacion
+
+public class TestMatrices {
+      public static void main(String[] args) {
+            // esto es object que va almacenar elementos de tipo entero
+            int edades[][] = new int[3][2];
+            System.out.println("edades = " + edades);
+      }
+}
