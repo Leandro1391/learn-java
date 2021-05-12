e21609742a0e7640c98e12a43bb781c6f40b0379
Leandro Julian <leandrojulian1391@gmail.com>
2021-05-11 21:40:10 -0300

    Acceder elementos de una matriz

--- Fundaments/Matrices/src/test/TestMatrices.java
+++ Fundaments/Matrices/src/test/TestMatrices.java
@@ -7,5 +7,15 @@ public class TestMatrices {
             // esto es object que va almacenar elementos de tipo entero
             int edades[][] = new int[3][2];
             System.out.println("edades = " + edades);
+            
+            edades[0][0] = 5;
+            edades[0][1] = 7;
+            edades[1][0] = 8;
+            edades[1][1] = 4;
+            
+            System.out.println("edades 0-0 = " + edades[0][0]);
+            System.out.println("edades 0-0 = " + edades[0][1]);
+            System.out.println("edades 0-0 = " + edades[1][0]);
+            System.out.println("edades 0-0 = " + edades[1][1]);
       }
 }
