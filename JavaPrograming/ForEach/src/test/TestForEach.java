ae8227893f32c2a4367ae4bd24323604f3abea2a
Leandro Julian <leandrojulian1391@gmail.com>
2021-05-16 21:28:04 -0300

    forEach in object Persona

--- JavaPrograming/ForEach/src/test/TestForEach.java
+++ JavaPrograming/ForEach/src/test/TestForEach.java
@@ -1,10 +1,19 @@
 package test;
 
+import domain.Persona;
+
 public class TestForEach {
+
       public static void main(String[] args) {
             int edades[] = {5, 6, 8, 9};
-            for(int edad : edades) {
+            for (int edad : edades) {
                   System.out.println("edad = " + edad);
+            }
+            
+            //Las variables de tipo hijo no puede guardar referencia del padre, en cambio, de tipo padre puede guardar referencia tanto de eĺ y sus hijos en la referencia de memoria
+            Persona personas[] = {new Persona("Giovanni"), new Persona("Julia"), new Persona("Salvatore")};
+            for (Persona persona : personas) {
+                  System.out.println("persona = " + persona);
             }
       }
 }
