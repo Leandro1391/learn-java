232f824334c645ee17de506fd3a8f6aea3f1bd5b
Leandro Julian <leandrojulian1391@gmail.com>
2021-05-19 17:54:58 -0300

    Formas para trabajar con instanceof

--- JavaPrograming/InstanceOf/src/domain/Gerente.java
+++ JavaPrograming/InstanceOf/src/domain/Gerente.java
@@ -14,4 +14,13 @@ public class Gerente extends Empleado {
       public String obtenerDetalles() {
             return super.obtenerDetalles() + ", departamento: " + this.departamento;
       }
+
+      public String getDepartamento() {
+            return this.departamento;
+      }
+
+      public void setDepartamento(String departamento) {
+            this.departamento = departamento;
+      }
+      
 }
