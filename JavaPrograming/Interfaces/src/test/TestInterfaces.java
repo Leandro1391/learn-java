7c681187238a80d420a2e0f615df9a78a48caf7c
Leandro Julian <leandrojulian1391@gmail.com>
2021-05-21 20:52:12 -0300

    Definicion de interface y cuando se usa

--- JavaPrograming/Interfaces/src/test/TestInterfaces.java
+++ JavaPrograming/Interfaces/src/test/TestInterfaces.java
@@ -2,6 +2,16 @@ package test;
 
 import accesodatos.*;
 
+/*Una interface es parecido a una clase abstracta, en el aspecto de que los metodos no tienen el comportamiento definido como metodo abstractos,
+ una interfaz es un nuevo tipo en java y similar a las clases abstractas todo ls metodo deinifdos seran abstractas y sus atritbuso sera constantes (public, final y static)
+
+La diferencia principal de una clase abstracta y una interfaz es:
+* Cuando las caracteristicas de nuestra clase son muy parecidas de una figura geometrica (cuadrado, triangulo, circulo) se usa herencia y la clase padres es abstracto
+* Cuando la relacion entre las clases tiene en común su comportamiento se recomienda usar INTERFACES como la conexion a distintas base de datos (MySQL y Oracle)
+para que tenga metodo genericos y los dos van implementar los metodos definidos en
+entonces euna interface  lo vamos a usar mas para el comportamiento de nuestras clases, No tanto por las caracteriscas que heredemos sino POR COMPORTAMIENTO EN COMUN
+*/
+
 //el objetivo de las interfaces es hacer metodos mas genericos
 //podremos asignar a la variables de tipo interface que puede apuntar a objetos que haya implementado la interfaz que necesitamos
 
