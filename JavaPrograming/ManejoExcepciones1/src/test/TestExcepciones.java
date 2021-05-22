fac8facfc72ec74aa693bd99fcfaa7accd1de2ae
Leandro Julian <leandrojulian1391@gmail.com>
2021-05-22 11:04:08 -0300

    First excersice

--- JavaPrograming/ManejoExcepciones1/src/test/TestExcepciones.java
+++ JavaPrograming/ManejoExcepciones1/src/test/TestExcepciones.java
@@ -1,14 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package test;
 
-/**
- *
- * @author leandro
- */
 public class TestExcepciones {
-      
+
+      public static void main(String[] args) {
+
+            int resultado = 0;
+
+            try {
+                  resultado = 10 / 0;
+            } catch (Exception e) {
+                  System.out.println("Exception  error: " + e);
+                  e.printStackTrace(System.out);
+            }
+
+            System.out.println("resultado = " + resultado);
+      }
 }
