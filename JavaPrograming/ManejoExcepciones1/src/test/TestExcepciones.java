5b304526b70bc901b08016877f2c2d3584547e78
Leandro Julian <leandrojulian1391@gmail.com>
2021-05-22 11:12:35 -0300

    Added comments

--- JavaPrograming/ManejoExcepciones1/src/test/TestExcepciones.java
+++ JavaPrograming/ManejoExcepciones1/src/test/TestExcepciones.java
@@ -5,12 +5,14 @@ public class TestExcepciones {
       public static void main(String[] args) {
 
             int resultado = 0;
-
+            
+            //Con try controlamos la excepcion para que no termine el programa de forma abrupta
             try {
-                  resultado = 10 / 0;
+                  resultado = 10 / 0; //esta es la linea que provoca la excepcion
+                 //capturamos la excepcion en el catch, el e es la variable que almacena  el tipo de excepcion
             } catch (Exception e) {
                   System.out.println("Exception  error: " + e);
-                  e.printStackTrace(System.out);
+                  e.printStackTrace(System.out); //Imprimimos la pila de expcepciones, porque puede haber una lista de excepciones
             }
 
             System.out.println("resultado = " + resultado);
