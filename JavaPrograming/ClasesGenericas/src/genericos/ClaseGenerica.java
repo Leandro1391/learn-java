66776891d278e46919212bbc4f26547356fa516c
Leandro Iadanza <leandrojulian1391@gmail.com>
2021-05-22 19:41:07 -0300

    definicion de tipo generico

--- JavaPrograming/ClasesGenericas/src/genericos/ClaseGenerica.java
+++ JavaPrograming/ClasesGenericas/src/genericos/ClaseGenerica.java
@@ -1,5 +1,14 @@
 package genericos;
 
+/*  Un tipo generico en java es un comodin, es decir, que en la clase no vamos a definir el tipo de dato que va almaenar sino en el momento
+que vamos a crear un objeto de la instancia de esta clase, se especificará el tipo de dato que a utilizar*/
+
+/* <E> Elemento
+<K> Llave
+<N> Number
+<T> type o tipo de una clase
+<V> representa un valor*/
+
 public class ClaseGenerica<T> {
       
       private T objecto;
