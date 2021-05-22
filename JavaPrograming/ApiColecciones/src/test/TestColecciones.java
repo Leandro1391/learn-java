2e15ba919fa8215b2194f14892f8cdfdab50833a
Leandro Iadanza <leandrojulian1391@gmail.com>
2021-05-22 15:45:18 -0300

    Listas con ArrayList()

---
+++ JavaPrograming/ApiColecciones/src/test/TestColecciones.java
@@ -0,0 +1,19 @@
+package test;
+
+import java.util.*;
+
+public class TestColecciones {
+      public static void main(String[] args) {
+            //una lista puede crecer dinamicamente
+            List miLista = new ArrayList();
+            miLista.add("Lunes");
+            miLista.add("Martes");
+            miLista.add("Miercoles");
+            miLista.add("Jueves");
+            miLista.add("Viernes");
+            miLista.add("Sabado");
+            miLista.add("Domingo");
+      }
+}
+
+/*Una lista tiene como propiedad agregar elementos de forma dinamica y ademas  mantiene el orden en el cual se guarda*/
