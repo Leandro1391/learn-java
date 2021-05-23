345b02c6a8543b4e078dc76a4fa12c06cd6ea2fb
Leandro Iadanza <leandrojulian1391@gmail.com>
2021-05-23 18:35:48 -0300

    Added main method static

--- JavaPrograming/CatalogoPeliculas/src/cpjlaboratoriofinal/CPJLaboratorioFinal.java
+++ JavaPrograming/CatalogoPeliculas/src/cpjlaboratoriofinal/CPJLaboratorioFinal.java
@@ -1,5 +1,55 @@
 package cpjlaboratoriofinal;
 
+import java.io.IOException;
+import java.util.Scanner;
+import mx.com.gm.peliculas.negocio.CatalogoPeliculasImpl;
+
 public class CPJLaboratorioFinal {
-      
+
+      public static void main(String[] args) throws IOException {
+
+            var console = new Scanner(System.in);
+
+            int opcion;
+            String nombreArchivo;
+            var catalogoPeliculas = new CatalogoPeliculasImpl();
+
+            do {
+                  System.out.println("Elige opcion: ");
+                  System.out.println("1 - Iniciar catalogo peliculas");
+                  System.out.println("2 - Agregar pelicula");
+                  System.out.println("3 - Listar Peliculas");
+                  System.out.println("4 - Buscar Pelicula");
+                  System.out.println("0 - Salir");
+                  
+                  try {
+                        opcion = Integer.parseInt(console.nextLine());
+                  } catch (NumberFormatException e) {
+                        e.printStackTrace(System.out);
+                        System.out.println(e.getMessage());
+                  } catch(Exception e) {
+                        e.printStackTrace(System.out);
+                  }finally {
+                        opcion = 5;
+                  }
+                  
+
+                  switch (opcion) {
+                        case 1:
+                              break;
+                        case 2:
+                              break;
+                        case 3:
+                              break;
+                        case 4:
+                              break;
+                        default:
+                              System.out.println("Opcion incorrectar vuelve a intentar");
+                              break;
+                  }
+                   
+                  System.out.println(" ");
+            } while (opcion != 0);
+      }
+
 }
--- JavaPrograming/CatalogoPeliculas/src/mx/com/gm/peliculas/datos/AccesoDatosImpl.java
+++ JavaPrograming/CatalogoPeliculas/src/mx/com/gm/peliculas/datos/AccesoDatosImpl.java
@@ -1,5 +1,36 @@
 package mx.com.gm.peliculas.datos;
 
-public class AccesoDatosImpl {
+import java.util.ArrayList;
+import mx.com.gm.peliculas.domain.Pelicula;
+
+public class AccesoDatosImpl implements IAccesoDatos{
+
+      public AccesoDatosImpl() {
+      }
+      
+      @Override
+      public boolean existe(String nombreArchivo) {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+      }
+
+      @Override
+      public ArrayList<Pelicula> listar(String nombre) {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+      }
+
+      @Override
+      public void escribir(Pelicula pelicula, String nombreArchivo, boolean anexar) {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+      }
+
+      @Override
+      public void crear(String nombreArchivo) {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+      }
+
+      @Override
+      public void borrar(String nombreArchivo) {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+      }
       
 }
