212dcd036adadf09a3e06d1b976dc66ba88981f9
Leandro Iadanza <leandrojulian1391@gmail.com>
2021-06-02 23:08:22 -0300

    Como realizar un pool de conexiones con apache-commons

---
+++ JDBC/ManejoJDBCPoolConexiones/src/main/java/datos/IUsuarioDao.java
@@ -0,0 +1,16 @@
+package datos;
+
+import domain.UsuarioDTO;
+import java.sql.SQLException;
+import java.util.List;
+
+public interface IUsuarioDao {
+      
+     List<UsuarioDTO> seleccionar() throws SQLException;
+     
+     int actualizar(UsuarioDTO usuario) throws SQLException;
+     
+     int insertar(UsuarioDTO usuario) throws SQLException;
+     
+     int eliminar(UsuarioDTO usuario) throws SQLException;
+}
